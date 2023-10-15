27aed8493d51cec4dd40701a40ceb6bee8a7434a
KKNMASTER <k.sakmongkoljit@gmail.com>
2023-10-15 22:45:09 +0700

    KKNHomeworkv2

--- Homework.dart
+++ Homework.dart
@@ -1,4 +1,3 @@
-import 'package:dart_application_2/dart_application_2.dart';
 // 1. given a list of numbers, find the sum of all the numbers
 void main() {
   List<int> numbers = [0,1,2,3,4,5,6,7,8,9];
